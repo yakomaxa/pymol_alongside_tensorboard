8f1cd537b2adc74dbf499e532390ec8742d9c553
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2021-06-28 02:09:51 +0900

    changed import

--- test_tb.py
+++ test_tb.py
@@ -1,14 +1,11 @@
 from torch.utils.tensorboard import SummaryWriter as sw
-###### We need to make writer before pymol launch for some (PyMOL) reason
+###### VERY IMPORTANT:We need to make writer before pymol launch for some (PyMOL) reason
 writer = sw("/Users/sakuma/tmpworkD/run_t")
-#sys.path.append("/Users/sakuma/mybin/pymoldssp/lib/python3.6/site-packages/")
-import tb_writer4structure_3
-tb_writer4structure_3.write_structure(writer=writer,
+import tb_writer4structure
+tb_writer4structure.write_structure(writer=writer,
                                     pdb="~/database/1MBN.pdb",step=0,quality=10)
 #tb_writer4structure.write_structure(writer=writer,
 #                                      pdb="~/database/4HP0.pdb",step=0,quality=10)
 #tb_writer4structure.write_structure(writer=writer,
  #                                   pdb="~/database/1CZU.pdb",step=1,quality=10)
 writer.close()
-
-#addstructur#e(daefile="/Users/sakuma/stick.dae", writer=writer, step=step)
